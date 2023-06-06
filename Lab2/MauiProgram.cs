bdbef7900c80886579ae2ce1a1053a86d24906e2
Daryna <79630199+drndrndrn@users.noreply.github.com>
2023-06-06 10:02:37 +0300

    Add files via upload

---
+++ Lab2/MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Compatibility.Hosting;
+namespace Lab2;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+            .UseMauiCompatibility()
+                .ConfigureMauiHandlers((handlers) => {
+#if ANDROID
+handlers.AddHandler(typeof(MauiApp.Controls.BorderedEntry),typeof(MauiApp.Platforms.Android.Renderers.BorderedEntryRenderer));
+#endif
+
+                });
+
+		return builder.Build();
+	}
+}
